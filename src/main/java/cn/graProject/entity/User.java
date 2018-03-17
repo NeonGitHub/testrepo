1bc51015a4bb5cc880963e435ed972e2962ca96a
zhangjingxuan <13609262628@163.com>
2018-03-17 22:27:36 +0800

    add sql&entity_user

--- src/main/java/cn/graProject/entity/User.java
+++ src/main/java/cn/graProject/entity/User.java
@@ -2,4 +2,51 @@ package cn.graProject.entity;
 
 public class User {
 
+	private String user_id;
+	
+	private String user_pwd;
+	
+	private String user_dev;
+	
+	private String user_email;
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUser_pwd() {
+		return user_pwd;
+	}
+
+	public void setUser_pwd(String user_pwd) {
+		this.user_pwd = user_pwd;
+	}
+
+	public String getUser_dev() {
+		return user_dev;
+	}
+
+	public void setUser_dev(String user_dev) {
+		this.user_dev = user_dev;
+	}
+
+	public String getUser_email() {
+		return user_email;
+	}
+
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [user_id=" + user_id + ", user_pwd=" + user_pwd + ", user_dev=" + user_dev + ", user_email="
+				+ user_email + "]";
+	}
+
+	
 }
--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -1,19 +1,37 @@
+--数据库初始化脚本
+--连接数据库
+mysql -uroot -p
+--创建数据库
+CREATE DATABASE fishbowl;
+--使用数据库
+use fishbowl;
+--创建表
 CREATE TABLE user_info(
 `user_id` varchar(120) NOT NULL COMMENT '用户名',
 `user_pwd` varchar(120) NOT NULL COMMENT '密码',
 `user_dev` varchar(120) NOT NULL COMMENT '设备号',
 `user_email` varchar(120) COMMENT '邮箱',
-`user_sex` char(1) NOT NULL COMMENT '性别',
 PRIMARY KEY (user_id),
-key idx_dev(user_dev),
+key idx_dev(user_dev)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
 
-
-CREATE TABLE device_info(
+CREATE TABLE device(
 `dev_id` varchar(120) NOT NULL COMMENT '设备号',
 `ph_data` double NOT NULL COMMENT 'PH值',
 `tds_data` double NOT NULL COMMENT 'TDS值',
 `temp_data` double NOT NULL COMMENT '温度值',
 `upload_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '传入时间',
 PRIMARY KEY (dev_id),
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='数据表'
+key idx_dev(dev_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='监控数据表';
+
+CREATE TABLE fish_case(
+`case_id` int NOT NULL COMMENT '案例编号',
+`fish_name` varchar(120) NOT NULL COMMENT '鱼的种类',
+`ph_min` double NOT NULL COMMENT '适应的最小的ph值',
+`ph_max` double NOT NULL COMMENT '适应的最大的ph值',
+`temp_min` double NOT NULL COMMENT '适应的最小温度值',
+`temp_max` double NOT NULL COMMENT '适应的最大温度值',
+`details` varchar(255) COMMENT '解决方案',
+PRIMARY KEY (case_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='鱼类案例表';
